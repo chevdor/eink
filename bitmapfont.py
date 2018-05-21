bb195931bd94b0b429d49f1548012e6f6e1c5215
chevdor <w.kopp@kigroup.de>
2018-05-21 21:04:19 +0200

    Add support for proportional fonts

--- bitmapfont.py
+++ bitmapfont.py
@@ -3,7 +3,6 @@
 # License: MIT License (https://opensource.org/licenses/MIT)
 import ustruct
 
-
 class BitmapFont:
     def __init__(self, width, height, pixel, font_name='font5x8.bin'):
         # Specify the drawing area width and height, and the pixel function to
@@ -38,28 +37,75 @@ class BitmapFont:
     def __exit__(self, exception_type, exception_value, traceback):
         self.deinit()
 
+    def get_char(self, ch):
+        char_data = []
+        for char_x in range(self._font_width):
+            self._font.seek(2 + (ord(ch) * self._font_width) + char_x)
+            line = ustruct.unpack('B', self._font.read(1))[0]
+            char_data.append(line)
+        return char_data
+
     def draw_char(self, ch, x, y, *args, **kwargs):
+        """ Draw a single char """
         # Don't draw the character if it will be clipped off the visible area.
         if x < -self._font_width or x >= self._width or \
            y < -self._font_height or y >= self._height:
             return
-        # Go through each column of the character.
-        for char_x in range(self._font_width):
+
+        data = self.get_char(ch)
+        boundaries = self.char_boundaries(ch)
+
+        # print("printing {} at x={}-{}".format(ch, x+boundaries[0], x+boundaries[1]))
+        # Go through each column of the character that contains pixels to draw
+        i = 0
+        for char_x in range(boundaries[0], boundaries[1] + 1):
             # Grab the byte for the current column of font data.
-            self._font.seek(2 + (ord(ch) * self._font_width) + char_x)
-            line = ustruct.unpack('B', self._font.read(1))[0]
-            # Go through each row in the column byte.
+            line = data[char_x]
+            
+            # Go through each non null row in the column byte.
             for char_y in range(self._font_height):
                 # Draw a pixel for each bit that's flipped on.
                 if (line >> char_y) & 0x1:
-                    self._pixel(x + char_x, y + char_y, *args, **kwargs)
+                    self._pixel(x + i, y + char_y, *args, **kwargs)
+            i += 1
 
     def text(self, text, x, y, *args, **kwargs):
-        # Draw the specified text at the specified location.
+        """ Draw the specified text at the specified location """
         for i in range(len(text)):
             self.draw_char(text[i], x + (i * (self._font_width + 1)), y,
                            *args, **kwargs)
 
-    def width(self, text):
-        # Return the pixel width of the specified text message.
+    # The goal here is to have less blank spaces when the chars take less room
+    # for instance, ... should be much narrower than WWW 
+    def text_proportional(self, text, x, y, *args, **kwargs):
+        """ Draw the specified text at the specified location """
+        pos = x
+        for i in range(len(text)):
+            ch = text[i]
+            # boundaries = self.char_boundaries(ch)
+            self.draw_char(ch, pos, y, *args, **kwargs)
+            pos += self.char_width(ch) + 1
+
+    def text_width(self, text):
+        """ Return the pixel width of the specified text message """
         return len(text) * (self._font_width + 1)
+
+    def char_boundaries(self, ch):
+        """ Returns the boundaries of a char """
+
+        index_start = index_end = -1
+        line = self.get_char(ch)
+        # Go through each column of the character.
+        for char_x in range(self._font_width):
+            # Grab the byte for the current column of font data.
+            if (line[char_x] > 0 and index_start == -1): index_start = char_x
+            if (line[char_x] > 0 and char_x > index_end):  index_end = char_x
+
+        return (index_start, index_end)
+
+    def char_width(self, ch):
+        """ Return the pixel width of the specified caracter """
+
+        boundaries = self.char_boundaries(ch)
+        if boundaries[0] < 0: return 2 # special case to increase slightly the spaces
+        return boundaries[1] - boundaries[0] +1
--- samples/ws2in9b_font_1.py
+++ samples/ws2in9b_font_1.py
@@ -1,5 +1,3 @@
-import random
-import time 
 import framebuf
 from machine import Pin, SPI
 import waveshare2in9b
@@ -14,24 +12,8 @@ buf_r = bytearray(DISPLAY_WIDTH * DISPLAY_HEIGHT // 8)
 fb_black = FrameBufferExtended(buf_b, DISPLAY_WIDTH, DISPLAY_HEIGHT, framebuf.MONO_HLSB)
 fb_red = FrameBufferExtended(buf_r, DISPLAY_WIDTH, DISPLAY_HEIGHT, framebuf.MONO_HLSB)
 
-black = 0
-white = 1
-
-def main(spi):
-	e = waveshare2in9b.EPD(spi, cs=32, dc=33, rst=25, busy=26, idle=0)
-	e.init()
-	fb_black.fill(white)
-	fb_red.fill(white)
-	fb_black.set_rotate(FrameBufferExtended.ROTATE_270)
-	fb_red.set_rotate(FrameBufferExtended.ROTATE_270)
-	MSG = 'It Works!'
-	with BitmapFont(fb_black.width, fb_black.height, fb_black.set_pixel) as bfb:	
-		with BitmapFont(fb_red.width, fb_red.height, fb_red.set_pixel) as bfr:	
-			# message_width = bfb.width(MSG)   # Message width in pixels.
-			bfb.text("It Works!",1,0,1)   
-			bfr.text("Awesome, now we can have fun!",1,10,1)       
-    
-			e.display_frame(buf_b, buf_r)
+BLACK = 0
+WHITE = 1
 
 # baudrate = int(20e6)
 baudrate = int(20e6)
@@ -45,5 +27,85 @@ spi = SPI(-1, baudrate=baudrate, polarity=0, phase=0, sck=Pin(14), mosi=Pin(13),
 # Comment out the 2 lines below if you want to use hardware SPI
 # hspi = SPI(1, baudrate=baudrate, polarity=0, phase=0, sck=Pin(14), mosi=Pin(13), miso=Pin(12))
 # spi = hspi
+e = waveshare2in9b.EPD(spi, cs=32, dc=33, rst=25, busy=26, idle=0)
 
-main(spi)
+def test1():
+	e.init()
+	fb_black.fill(WHITE)
+	fb_red.fill(WHITE)
+	fb_black.set_rotate(FrameBufferExtended.ROTATE_270)
+	fb_red.set_rotate(FrameBufferExtended.ROTATE_270)
+	with BitmapFont(fb_black.width, fb_black.height, fb_black.set_pixel) as bfb:	
+		with BitmapFont(fb_red.width, fb_red.height, fb_red.set_pixel) as bfr:	
+			for i in range (6):
+				for j in range(45):
+					val = i * 45 + j 
+					if val < 256:
+						# print("print char", val)
+						if i % 2:
+							bfb.draw_char(chr(val),j * (bfb._font_width +1 ),i * (bfb._font_height +1),1)   
+						else:
+							bfr.draw_char(chr(val),j * (bfr._font_width +1 ),i * (bfr._font_height +1),1)   
+
+	e.display_frame(buf_b, buf_r)
+
+def test2():
+	e.init()
+	fb_black.fill(WHITE)
+	fb_red.fill(WHITE)
+	fb_black.set_rotate(FrameBufferExtended.ROTATE_270)
+	fb_red.set_rotate(FrameBufferExtended.ROTATE_270)
+	MSG = 'It Works!'
+	with BitmapFont(fb_black.width, fb_black.height, fb_black.set_pixel) as bfb:	
+		with BitmapFont(fb_red.width, fb_red.height, fb_red.set_pixel) as bfr:	
+			# message_width = bfb.width(MSG)   # Message width in pixels.
+			bfb.text("It Works!",1,0,1)   
+			bfr.text("Awesome, now we can have fun!",1,10,1)       
+	
+			e.display_frame(buf_b, buf_r)
+
+def test3():
+	""" Test char boundaries """
+	e.init()
+	fb_black.fill(WHITE)
+	fb_black.set_rotate(FrameBufferExtended.ROTATE_270)
+	with BitmapFont(fb_black.width, fb_black.height, fb_black.set_pixel) as bfb:	
+		for c in ['A', '.', '-', ':', ' ']:
+			print (c, '->',bfb.char_boundaries(c), bfb.char_width(c))   
+				
+# bfb = BitmapFont(fb_black.width, fb_black.height, fb_black.set_pixel); bfb.init()
+# bfb.char_boundaries('.')
+
+def test4():
+	e.init()
+	fb_black.fill(WHITE); fb_red.fill(WHITE)
+
+	fb_black.set_rotate(FrameBufferExtended.ROTATE_270)
+	with BitmapFont(fb_black.width, fb_black.height, fb_black.set_pixel) as bfb:	
+		MSG = "AA...-::ZBCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		bfb.text(MSG,0,0,1)   		
+		bfb.text_proportional(MSG,0,10,1) 
+		MSG = 'The quick brown fox jumps over the lazy dog'  
+		bfb.text(MSG,0,20,1)   		
+		bfb.text_proportional(MSG,0,30,1) 
+
+	e.display_frame(buf_b, buf_r)
+
+def test5():
+	e.init()
+	fb_black.fill(WHITE)
+	fb_red.fill(WHITE)
+	fb_black.set_rotate(FrameBufferExtended.ROTATE_270)
+	fb_red.set_rotate(FrameBufferExtended.ROTATE_270)
+	with BitmapFont(fb_black.width, fb_black.height, fb_black.set_pixel) as bfb:	
+		with BitmapFont(fb_red.width, fb_red.height, fb_red.set_pixel) as bfr:	
+			bfb.text("ABCDEFGHIJKLMNOPQRSTUVWXYZ",1,0,1)   
+			bfr.text("abcdefghijklmnopqrstuvwxyz",1,10,1)       
+			bfb.text("0123456789.",1,20,1)       
+			
+			bfr.text_proportional("ABCDEFGHIJKLMNOPQRSTUVWXYZ",1,30,1)   
+			bfb.text_proportional("abcdefghijklmnopqrstuvwxyz",1,40,1)       
+			bfr.text_proportional("0123456789.",1,50,1)       
+	
+			e.display_frame(buf_b, buf_r)
+
